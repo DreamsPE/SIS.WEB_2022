a4bdec5d4c89ca78aea0ead73b9612d02fa28139
DreamsPE <104179084+DreamsPE@users.noreply.github.com>
2022-07-15 00:34:47 -0500

    .
    .

--- private/controllers/ErrorController.php
+++
@@ -1,9 +0,0 @@
-<?php
-class ErrorController extends Controller{
-    public function index()
-	{
-		// code...
-		$this->view('home\404');
-	}
-
-}
--- public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-//require_once("../private/core/app.php");
-require("../private/core/autoload.php");
-
-    $app = new App();
-?>

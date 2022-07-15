4412fd71c8b6c6164c16f13a11d88fae5823da06
DreamsPE <104179084+DreamsPE@users.noreply.github.com>
2022-07-15 00:58:07 -0500

    git commit --interactive

--- private/core/config.php
+++ private/core/config.php
@@ -3,7 +3,7 @@ define('ROOT', 'http://localhost/PROYECT_IWEB2022/public');
 define('ASSETS', 'http://localhost/PROYECT_IWEB2022/public/assets');
 
 
-define('DBNAME', 'rrrrr');
+define('DBNAME', 'sisweb2022');
 define('DBHOST', 'localhost');
 define('DBUSER', 'root');
 define('DBPASS', 'Nova|43886031%25');

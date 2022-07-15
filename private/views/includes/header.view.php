cc991ac80009858c80c978154a9468490f423076
DreamsPE <hchumpitaza@ucvvirtual.edu.pe>
2022-07-14 23:38:42 -0500

    My commit message

---
+++ private/views/includes/header.view.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>GRAN AMAUTA DE MOTUPE</title>
+        <!-- BOOTSTRAP STYLES-->
+        <link href="<?=ASSETS?>/css/bootstrap.css" rel="stylesheet" />
+        <!-- FONTAWESOME STYLES-->
+        <link href="<?=ASSETS?>/css/fontawesome.css" rel="stylesheet" />
+        <!--CUSTOM BASIC STYLES-->
+        <link href="<?=ASSETS?>/css/basic.css" rel="stylesheet" />
+        <!--CUSTOM MAIN STYLES-->
+        <link href="<?=ASSETS?>/css/custom.css" rel="stylesheet" />
+        <!-- GOOGLE FONTS-->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+        <script src="<?=ASSETS?>/js/jquery-1.10.2.js"></script>
+        </head>
+
+        <div id="wrapper">
+        <!-- /. WRAPPER (includes/header) -->
+        <nav class="navbar navbar-default navbar-cls-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="<?=ROOT?>/home">I.E.P. "GRAN AMAUTA DE MOTUPE"</a>
+            </div>
+        </nav>
+        <!-- /. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+                    <li>
+                    <a class="active-menu" href="index.php"><i class="fa fa-uniF17A"></i>Principal</a>
+                    </li>
+					<li>
+                     
+                    <a href="<?=ROOT?>/courses"><i class="fa fa-uniF07C "></i>Cursos</a>
+
+                    </li>
+					
+					<li>
+                        <a href="<?=ROOT?>/students"><i class="fa fa-uniF0C0 "></i>Estudiantes</a>
+                    </li>
+					<li>
+                        <a href="fees.php"><i class="fa fa-uniF19D "></i>Matricula</a>
+                    </li>
+					 <li>
+                        <a href="report.php"><i class="fa fa-uniF15C "></i>Reportes</a>
+                    </li>
+					
+					 
+					
+					<li>
+                        <a href="setting.php"><i class="fa fa-uniF085 "></i>Configuracion</a>
+                    </li>
+					
+					 <li>
+                        <a href="logout.php"><i class="fa fa-uniF011 "></i>Salir</a>
+                    </li>
+					
+			
+                </ul>
+
+            </div>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
+    <div id="page-wrapper">
+    <!-- /. PAGE WRAPPER  -->
+        <div id="page-inner">
+        <!-- /. PAGE INNER  -->
---
+++ private/views/students/index.view.php
@@ -0,0 +1,17 @@
+
+
+<?php
+$this->view('includes/header')
+?>
+
+<div class="col-md-4">
+                        <div class="main-box mb-pink">
+                            <a href="<?=ROOT?>/students/create">
+                                <i class="fa fa-users fa-5x"></i>
+                                <h5>Estudiantes</h5>
+                            </a>
+                        </div>
+                    </div>
+<?php
+$this->view('includes/footer')
+?>
